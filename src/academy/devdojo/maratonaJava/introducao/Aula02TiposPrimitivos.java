e69445895f9c0aed3b5c6aee1c888e7c969d01fc
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-06-24 09:41:38 -0300

    062425 11

--- src/academy/devdojo/maratonaJava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonaJava/introducao/Aula02TiposPrimitivos.java
@@ -3,6 +3,17 @@ package academy.devdojo.maratonaJava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         int idade = 10;
+        long numeroLongo = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 87;
+
         System.out.println("A idade é" + idade + " anos");
+        System.out.println(verdadeiro);
+        System.out.println("char" + caractere);
     }
 }
