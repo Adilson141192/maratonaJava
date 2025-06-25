c9319d26939ca78356c11dd2aceb41f67ff54722
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-06-25 10:17:07 -0300

    062525 14

---
+++ src/academy/devdojo/maratonaJava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonaJava.introducao;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String nome = "Kirito";
+        String endereco = "Av Brasil";
+        double salario = 5431.12;
+        String dataRecebimentoSalario = "20/12/2021";
+        String relatorio = "Eu, " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salário de R$" + salario + ", na data de " + dataRecebimentoSalario;
+
+        System.out.println(relatorio);
+    }
+}
