efb134d3331b90ad708ebb3e02aa281757469da7
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-07-17 14:57:31 -0300

    071725 aula16

--- src/academy/devdojo/maratonaJava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonaJava/introducao/Aula04Operadores.java
@@ -2,10 +2,26 @@ package academy.devdojo.maratonaJava.introducao;
 
 public class Aula04Operadores {
     public static void main(String[] args) {
+        // + - / *
         int numero01 = 10;
         int numero02 = 20;
-        int resultado = numero01 / numero02;
-
+        double resultado = numero01 / (double) numero02;
         System.out.println(resultado);
+
+        //%
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualAVinte = 10 == 20;
+        boolean isDezIgualADez = 10 == 10;
+        boolean isDezDiferenteADez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte = " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte = " + isDezMenorQueVinte);
+        System.out.println("isDezIgualAVinte = " + isDezIgualAVinte);
+        System.out.println("isDezIgualADez = " + isDezIgualADez);
+        System.out.println("isDezDiferenteADez = " + isDezDiferenteADez);
     }
 }
