b814dc5a7301592ee780250136fce67543187cc8
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-07-18 10:00:48 -0300

    071825 aula21

--- src/academy/devdojo/maratonaJava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonaJava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,13 +1,16 @@
 package academy.devdojo.maratonaJava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
-        int idade = 10;
+        int idade = 15;
         boolean isAutorizado = idade >= 18;
         // !
-        if(isAutorizado) {
+        if(isAutorizado != false) {
             System.out.println("Autorizado a compra bebida alcoólica");
+        } else {
+            System.out.println("Não autorizado a compra bebida defeito");
         }
+
         if(!isAutorizado) {
             System.out.println("Não autorizado a compra bebida defeito");
         }
