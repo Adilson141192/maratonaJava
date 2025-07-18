b5ba005e64efde4e025669103acb7681207b6b93
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-07-18 09:34:16 -0300

    071825 aula19

--- src/academy/devdojo/maratonaJava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonaJava/introducao/Aula04Operadores.java
@@ -41,5 +41,25 @@ public class Aula04Operadores {
         boolean isPlayStationCincoCompravel =
                 valorTotalContaCorrente >  valorPlayStation || valorTotalContaPoupanca > valorPlayStation;
         System.out.println("isPlayStationCincoCompravel =  " + isPlayStationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(++contador2);
+        System.out.println(contador2);
     }
 }
