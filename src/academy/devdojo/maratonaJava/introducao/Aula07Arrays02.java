c4753b6145c1ca9e7478a46ce29ab9a8dcaddf1f
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-07-22 15:48:19 -0300

    072225 aula33

---
+++ src/academy/devdojo/maratonaJava/introducao/Aula07Arrays02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonaJava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // valores padrão
+        // byte, short, int, long, float e double = 0
+        // char = '/u0000' ' '
+        // boolean = false
+        // String = null
+        String [] idades = new String[3];
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
