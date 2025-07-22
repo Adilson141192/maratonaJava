888be40e48fe2dc23efb18977e2b82892e89c1c7
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-07-22 15:59:46 -0300

    072225 aula34

--- src/academy/devdojo/maratonaJava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonaJava/introducao/Aula07Arrays02.java
@@ -7,10 +7,13 @@ public class Aula07Arrays02 {
         // char = '/u0000' ' '
         // boolean = false
         // String = null
-        String [] idades = new String[3];
+        String [] nomes = new String[3];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
 
-        System.out.println(idades[0]);
-        System.out.println(idades[1]);
-        System.out.println(idades[2]);
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
     }
 }
